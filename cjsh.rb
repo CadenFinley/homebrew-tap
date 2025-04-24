460054da1a5dffb23196179741f508c49e743309
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-04-24 16:29:47 -0500

    Update cjsh.rb

--- cjsh.rb
+++ cjsh.rb
@@ -2,8 +2,8 @@ class Cjsh < Formula
   desc "CJ's Shell"
   homepage "https://github.com/CadenFinley/CJsShell"
   url "https://github.com/CadenFinley/CJsShell.git",
-      tag:      "2.0.2.7",
-      revision: "496b6f261f524ede08412d409f3c3b1fc1c9e14e"
+      tag:      "2.0.2.8",
+      revision: "c57226cae9146e483c5fec0d963431798b2f1c4b"
 
   license "MIT"
 
@@ -31,9 +31,25 @@ class Cjsh < Formula
     <<~EOS
       CJ's Shell
       To set as your default shell run 'cjsh --set-as-shell'
+       or run 'chsh -s #{opt_bin}/cjsh'
       To see the help menu run 'cjsh --help'
-      To launch cjsh as a login shell run 'cjsh --login'
     EOS
+  end
+
+  def post_uninstall
+    original_shell_file = var/"cjsh"/"original_shell.txt"
+    if original_shell_file.exist?
+      original_shell = original_shell_file.read.strip
+      if original_shell.present? && File.executable?(original_shell)
+        system "chsh", "-s", original_shell
+        ohai "Your shell has been restored to: #{original_shell}"
+        ohai "Please restart your terminal for the changes to take effect."
+      else
+        opoo "Could not restore original shell: Invalid shell path found in #{original_shell_file}"
+      end
+    else
+      opoo "Could not restore original shell: No record of original shell found"
+    end
   end
 
   test do
