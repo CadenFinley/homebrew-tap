ec938fe67011efad7112fa4974494438d51fe3dc
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-04-24 13:20:36 -0500

    new brew

--- cjsh.rb
+++ cjsh.rb
@@ -2,26 +2,24 @@ class Cjsh < Formula
   desc "CJ's Shell"
   homepage "https://github.com/CadenFinley/CJsShell"
   url "https://github.com/CadenFinley/CJsShell.git",
-      tag:      "2.0.2.4",
-      revision: "7daa3f941777f8d8fd75a0130714b1ff70597859"
-  version "2.0.2.4"
+      tag:      "2.0.3.1",
+      revision: "22fcd2ef9ed81c36df7df369de30f06878db1db0"
+  version "2.0.3.1"
 
   license "MIT"
 
   depends_on "cmake" => :build
-  depends_on "openssl@3"
   depends_on "pkg-config" => :build
+  depends_on "openssl@3"
+  depends_on "curl"
+  depends_on "nlohmann-json"
 
   def install
-    mkdir "build" do
-      system "cmake", "..",
-             "-DCMAKE_BUILD_TYPE=Release",
-             "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-             "-DCMAKE_PREFIX_PATH=#{Formula["openssl@3"].opt_prefix}",
-             *std_cmake_args
-      system "make"
-      bin.install "cjsh"
-    end
+    system "cmake", "-S", ".", "-B", "build",
+           "-DCMAKE_PREFIX_PATH=#{Formula["openssl@3"].opt_prefix}",
+           *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   def post_install
@@ -29,14 +27,16 @@ class Cjsh < Formula
     (prefix/"original_shell.txt").write original_shell
   end
 
-  def post_uninstall
-    original = (prefix/"original_shell.txt").read.chomp rescue nil
-    return if original.to_s.empty?
-    ohai "Restoring your original shell to #{original}"
-    safe_system "sudo", "chsh", "-s", original, ENV["USER"]
-  end
-
   test do
     assert_match "v#{version}", shell_output("#{bin}/cjsh --version")
+  end
+
+  def caveats
+    <<~EOS
+      CJ's Shell
+      To set as your default shell run 'cjsh --set-as-shell'
+      To see the help menu run 'cjsh --help'
+      To launch cjsh as a login shell run 'cjsh --login'
+    EOS
   end
 end
