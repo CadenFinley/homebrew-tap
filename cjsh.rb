16b92165f42765bf7f23693d37c0372b5db142d3
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-09-04 19:24:49 -0500

    Update cjsh.rb

--- cjsh.rb
+++ cjsh.rb
@@ -3,8 +3,8 @@ class Cjsh < Formula
   homepage "https://github.com/CadenFinley/CJsShell"
   license "MIT"
   url "https://github.com/CadenFinley/CJsShell.git",
-      tag:      "2.3.8",
-      revision: "38d044d959891697698ca2d5e630553bd08133a9"
+      tag:      "2.3.9",
+      revision: "65765c02a1e9067e712d52b824ed56cd419cfbd7"
   head "https://github.com/CadenFinley/CJsShell.git", branch: "master"
 
   depends_on "cmake"      => :build
