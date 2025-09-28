313888110c61f57711d232766266b4e22a5db2da
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-09-27 20:18:00 -0500

    new formulae

--- cjsh.rb
+++ cjsh.rb
@@ -1,99 +1,63 @@
 class Cjsh < Formula
-  desc "CJ's Shell - Modern, interactive shell with AI assistance"
+  desc "CJ's Shell (cjsh) is a lightweight shell with out of the box power and speed."
   homepage "https://github.com/CadenFinley/CJsShell"
   license "MIT"
   url "https://github.com/CadenFinley/CJsShell.git",
-      tag:      "3.1.1",
+      tag:      "3.5.4",
       revision: "bbd95ace677d8833ce7074a5cc3b8256f26cd069"
   head "https://github.com/CadenFinley/CJsShell.git", branch: "master"
 
-  depends_on "cmake"      => :build
-  depends_on "curl"
-  depends_on "nlohmann-json"
-
   def install
-    ohai "Configuring CJ's Shell with CMake..."
-    args = std_cmake_args + %W[
-      -DCMAKE_FIND_FRAMEWORK=LAST
-      -DBUILD_TESTS=OFF
-    ]
-    mkdir "build" unless Dir.exist?("build")
-    system "cmake", "-S", ".", "-B", "build", *args
-    ohai "Building CJ's Shell..."
-    system "cmake", "--build", "build", "--config", "Release", "-j#{ENV.make_jobs}"
-    ohai "Installing CJ's Shell..."
-    system "cmake", "--install", "build"
-    ohai "Installing man pages..."
-    man1.install "man/cjsh.1"
-    (prefix/"plugins").mkpath
-    pkgshare.mkpath
-    cp Dir["themes/*.json"], pkgshare
-    ohai "Setting up environment..."
-    (var/"cjsh").mkpath
-    original_file = var/"cjsh"/"original_shell"
-    original_file.write ENV["SHELL"] unless original_file.exist?
-    ohai "Installation complete!"
+    # Build using the nob build system
+    cd "build_tools" do
+      system "cc", "-o", "nob", "nob.c"
+      system "./nob"
+    end
+
+    # Install the binary
+    bin.install "build/cjsh"
+  end
+
+  def uninstall
+    # Remove the binary
+    (bin/"cjsh").unlink if (bin/"cjsh").exist?
+    
+    # Remove from /etc/shells if it exists there
+    if File.exist?("/etc/shells") && File.read("/etc/shells").include?("#{bin}/cjsh")
+      ohai "Removing #{bin}/cjsh from /etc/shells"
+      system "sudo", "sed", "-i", "", "/#{bin.to_s.gsub("/", "\\/")}\\/cjsh/d", "/etc/shells"
+    end
+    
+    # Inform user about additional files
+    ohai "Additional files that can be manually removed:"
+    puts "  ~/.config/cjsh/          (configuration directory)"
+    puts "  ~/.cache/cjsh/        (cache directory)"
+    puts "  ~/.cjprofile          (profile file)"
+    puts "  ~/.cjshrc             (rc file)"
+    puts ""
+    puts "If cjsh was set as your login shell, change it back with:"
+    puts "  chsh -s /bin/bash  # or /bin/zsh"
   end
 
   def caveats
-    version_info = "development version"
-    version_info = "v#{version}" if build.stable?
     <<~EOS
-      #{'   ______       __   _____    __  __'}
-      #{'  / ____/      / /  / ___/   / / / /'}
-      #{' / /      __  / /   \\__ \\   / /_/ / '}
-      #{'/ /___   / /_/ /   ___/ /  / __  /  '}
-      #{'\\____/   \\____/   /____/  /_/ /_/   '}
-      #{'  CJ\'s Shell v' + (build.stable? ? version.to_s : "dev")}
+      To use cjsh as your login shell:
+      1. Add #{bin}/cjsh to /etc/shells:
+         echo "#{bin}/cjsh" | sudo tee -a /etc/shells
 
-      Warning: cjsh is 90% POSIX compliant but has known edge cases.
-       Misuse of cjsh or incorrectly setting it as your login shell 
-       can have adverse effects and there is NO warranty.
+      2. Change your shell:
+         chsh -s #{bin}/cjsh
 
-      To add CJsShell to the list of allowed shells run:
-        sudo sh -c "echo #{opt_bin}/cjsh >> /etc/shells"
-      To set as your default shell run:
-        chsh -s #{opt_bin}/cjsh
-      To see the help menu run:
-        cjsh --help
-      For more information, visit:
-        #{homepage}
+      Configuration files will be created in ~/.config/cjsh/ on first run.
+      To uninstall cjsh configuration, run: cjsh -c "cjsh_uninstall"
     EOS
   end
 
-  def post_upgrade
-    if build.stable?
-      ohai "CJ's Shell has been upgraded to v#{version}!"
-      ohai "Changes from this version:"
-      ohai "- Check #{homepage}/releases/tag/#{version} for release notes"
-    else
-      ohai "CJ's Shell has been upgraded to the latest development version!"
-      ohai "- Check #{homepage}/commits/master for recent changes"
-    end
-    ohai "Please restart your terminal session for the changes to take effect."
-  end
-
-  def post_uninstall
-    original_shell_file = var/"cjsh"/"original_shell"
-    if original_shell_file.exist?
-      original_shell = original_shell_file.read.strip
-      if !original_shell.empty? && File.executable?(original_shell)
-        system "chsh", "-s", original_shell
-        ohai "Your shell has been restored to: #{original_shell}"
-        ohai "Please restart your terminal for the changes to take effect."
-        original_shell_file.unlink
-      else
-        opoo "Could not restore original shell: Invalid shell path found in #{original_shell_file}"
-      end
-    else
-      opoo "Could not restore original shell: No record of original shell found"
-    end
-    rm_rf var/"cjsh"
-  end
-
   test do
-    assert_match "v", shell_output(bin/"cjsh --version")
-    system bin/"cjsh", "-c", "echo hello"
-    assert_equal "hello", shell_output("#{bin}/cjsh -c 'echo hello'").strip
+    # Test that the binary runs and shows version
+    assert_match "cjsh", shell_output("#{bin}/cjsh --version 2>&1")
+    
+    # Test basic command execution
+    assert_match "hello", shell_output("#{bin}/cjsh -c 'echo hello'")
   end
 end
