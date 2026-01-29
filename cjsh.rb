1e28b0112ec0ca9175224fe2d4e6a4e551635951
CadenFinley <caden.finley0789@gmail.com>
2026-01-28 21:00:45 -0600

    Update cjsh.rb

--- cjsh.rb
+++ cjsh.rb
@@ -1,46 +1,49 @@
 class Cjsh < Formula
-  desc "POSIX Shell Scripting meets Modern Shell Features."
+  desc "POSIX Shell Scripting meets Modern Shell Features"
   homepage "https://github.com/CadenFinley/CJsShell"
+  url "https://github.com/CadenFinley/CJsShell/archive/refs/tags/v1.1.3.tar.gz"
+  sha256 "03d62cd504ab5fb56d9790546bc6448e357d7637dab55489f52358910a69c023"
   license "MIT"
-  url "https://github.com/CadenFinley/CJsShell.git",
-      tag:      "v1.1.3",
-      revision: "17ad981a6c20ef267d579cd1e94a80dff9338019"
   head "https://github.com/CadenFinley/CJsShell.git", branch: "master"
 
+  STABLE_GIT_HASH = "17ad981a".freeze
+
   depends_on "cmake" => :build
+
+  conflicts_with "cjsh-dev", because: "both install `cjsh` binaries"
 
   def install
     git_hash = begin
       if (buildpath/".git").directory?
         Utils.safe_popen_read("git", "-C", buildpath, "rev-parse", "--short", "HEAD").strip
-      elsif stable? && stable.specs[:revision]
-        stable.specs[:revision][0, 7]
+      elsif stable?
+        self.class::STABLE_GIT_HASH
       else
         version.to_s
       end
     rescue
-      stable? && stable.specs[:revision] ? stable.specs[:revision][0, 7] : version.to_s
+      stable? ? self.class::STABLE_GIT_HASH : version.to_s
     end
 
-    git_hash = "unknown" if git_hash.nil? || git_hash.empty?
+    git_hash = "unknown" if git_hash.blank?
     ENV["CJSH_GIT_HASH_OVERRIDE"] = git_hash
 
     args = std_cmake_args + ["-DCMAKE_BUILD_TYPE=Release"]
     system "cmake", "-S", ".", "-B", "build", *args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-    
+
     system "#{bin}/cjsh", "--version"
   end
 
   def uninstall
     (bin/"cjsh").unlink if (bin/"cjsh").exist?
-    
+
     if File.exist?("/etc/shells") && File.read("/etc/shells").include?("#{bin}/cjsh")
       ohai "Removing #{bin}/cjsh from /etc/shells"
       system "sudo", "sed", "-i", "", "/#{bin.to_s.gsub("/", "\\/")}\\/cjsh/d", "/etc/shells"
     end
-    
+
     ohai "Additional files that can be manually removed:"
     puts "  ~/.cache/cjsh/        (cache directory)"
     puts "  ~/.cjprofile          (profile file)"
@@ -53,7 +56,7 @@ class Cjsh < Formula
 
   test do
     assert_match "cjsh", shell_output("#{bin}/cjsh --version 2>&1")
-    
+
     assert_match "hello", shell_output("#{bin}/cjsh -c 'echo hello'")
   end
 end
